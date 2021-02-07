4fd445173ff52a58c199951d5947304fbda9c4f0
miraclestyle <elvinkosova@gmail.com>
2021-02-07 07:22:32 +0000

    Code clean-up

--- backend/server/router/locations.js
+++ backend/server/router/locations.js
@@ -1,21 +1,8 @@
 const express = require('express');
+const requestHandler = require('./request_handler');
 const db = require('../database');
 
 const router = express.Router();
-
-const requestHandler = (query, argsFormater) => {
-  return (req, res) => {
-    query(...argsFormater(req))
-      .then((data) => {
-        console.log(`${query.name} data:`, data);
-        res.status(200).json(data);
-      })
-      .catch((error) => {
-        console.log(`${query.name} error:`, error);
-        res.status(500).end('Something went wrong. Please try again later.');
-      });
-  };
-};
 
 const getLocations = (req) => ([req.query.parent_id]);
 const getLocation = (req) => ([req.params.id]);
@@ -24,7 +11,10 @@ const insertLocation = (req) => ([
   req.body.parent_id,
   req.body.description,
 ]);
-const updateLocation = (req) => ([req.params.id, req.body.name, req.body.description]);
+const updateLocation = (req) => ([
+  req.params.id, req.body.name,
+  req.body.description,
+]);
 
 router.get('/api/locations', requestHandler(db.getLocations, getLocations));
 router.get('/api/locations/:id', requestHandler(db.getLocation, getLocation));
