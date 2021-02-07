c96588c2694fd47f6d2936ec9c1b56b0655fbd87
miraclestyle <elvinkosova@gmail.com>
2021-02-07 07:01:20 +0000

    Implement product movement API

--- backend/server/database/queries.js
+++ backend/server/database/queries.js
@@ -1,7 +1,9 @@
 const products = require('./products');
 const locations = require('./locations');
+const movements = require('./movements');
 
 module.exports = {
   ...products,
   ...locations,
+  ...movements,
  };
