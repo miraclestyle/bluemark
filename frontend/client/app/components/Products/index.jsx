ccbbdfeb50f3aecc2bc3dcad63357d04ed17a82b
miraclestyle <elvinkosova@gmail.com>
2021-02-08 10:28:48 +0000

    Reorganize code

--- frontend/client/app/components/Products/index.jsx
+++ frontend/client/app/components/Products/index.jsx
@@ -1,20 +1,20 @@
 const React = require('react');
-const api = require('./backendAdapter');
+const api = require('../backendAdapter');
 const ProductsList = require('./ProductsList.jsx');
 
-class Locations extends React.Component {
+class Products extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      locations: [],
-      location: {id: null, parent_id: null, path: '', name: '', description: ''},
+      products: [],
+      product: {id: null, name: '', description: ''},
     };
-    this.getProducts = this.getProducts.bind(this);
-    this.updateProduct = this.updateProduct.bind(this);
     this.newProduct = this.newProduct.bind(this);
+    this.updateProduct = this.updateProduct.bind(this);
     this.editProduct = this.editProduct.bind(this);
-    this.modifyProduct = this.modifyProduct.bind(this);
-    this.updateProductsList = this.updateProductsList.bind(this);
+    this.updateProducts = this.updateProducts.bind(this);
+    this.getProducts = this.getProducts.bind(this);
+    this.saveProduct = this.saveProduct.bind(this);
   }
 
   componentDidMount() {
@@ -25,12 +25,11 @@ class Locations extends React.Component {
     this.setState((state) => {
       const products = state.products.slice();
       products.push({id: null, name: '', description: ''});
-      console.log(products);
       return { products };
     });
   }
 
-  modifyProduct(selectedProduct) {
+  updateProduct(selectedProduct) {
     const newProduct = {
       id: selectedProduct.id,
       name: selectedProduct.name,
@@ -53,7 +52,7 @@ class Locations extends React.Component {
     });
   }
 
-  updateProductsList(product_id, newProduct) {
+  updateProducts(product_id, newProduct) {
     this.setState((state) => {
       const products = state.products.slice();
       const i = products.findIndex((product) => (product.id === product_id));
@@ -62,70 +61,47 @@ class Locations extends React.Component {
     });
   }
 
-  getProducts(limit = 1000, offset = 0) {
-    api.getProducts(limit, offset, (records) => {
+  getProducts() {
+    api.getProducts(1000, 0, (records) => {
       this.setState(() => ({ products: records }));
     });
   }
 
-  updateProduct() {
+  saveProduct() {
     const { product } = this.state;
     if (product.id !== null) {
       api.updateProduct(
         product.id,
         product.name,
         product.description,
-        (records) => (this.updateProductsList(product.id, records[0]))
+        (records) => (this.updateProducts(product.id, records[0]))
       );
     } else {
       api.insertProduct(
         product.name,
         product.description,
-        (records) => (this.updateProductsList(null, records[0]))
+        (records) => (this.updateProducts(null, records[0]))
       );
     }
   }
 
-  getLocations(parent_id = null) {
-    api.getLocations(parent_id);
-  }
-
-  getLocation(location_id) {
-    api.getLocation(location_id);
-  }
-
-  insertLocation(parent_id, name, description) {
-    api.insertLocation(parent_id, name, description);
-  }
-
-  updateLocation(location_id, name, description) {
-    api.updateLocation(location_id, name, description);
-  }
-
-  getMovements(product_id, location_path) {
-    api.getMovements(product_id, location_path);
-  }
-
-  insertMovementEntries(product_id, entries) {
-    api.insertMovementEntries(product_id, entries);
-  }
-
   render() {
     const { products, product } = this.state;
-    const { newProduct, modifyProduct, editProduct, updateProduct } = this;
+    const { newProduct, updateProduct, editProduct, saveProduct } = this;
     return (
       <div>
+        <h3>Products</h3>
+        <button onClick={newProduct}>Add New Product</button>
         <ProductsList
           products={products}
-          selectedProduct={product}
-          newProduct={newProduct}
-          modifyProduct={modifyProduct}
-          editProduct={editProduct}
+          selected={product}
           updateProduct={updateProduct}
+          editProduct={editProduct}
+          saveProduct={saveProduct}
         />
       </div>
     );
   }
 }
 
-module.exports = Locations;
+module.exports = Products;
