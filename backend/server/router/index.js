5659fac145bcbc129851ae3dee458c9e4bdc57f1
miraclestyle <elvinkosova@gmail.com>
2021-02-08 03:19:07 +0000

    Implement frontend to backend interface

--- backend/server/router/index.js
+++ backend/server/router/index.js
@@ -14,6 +14,8 @@ router.use((req, res, next) => {
 
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
--- frontend/client/config/index.js
+++ frontend/client/config/index.js
@@ -2,3 +2,4 @@ module.exports.FRONTEND_PORT = 8888;
 module.exports.BACKEND_ENDPOINT = 'http://localhost:8080/api/';
 module.exports.PRODUCTS = 'products';
 module.exports.LOCATIONS = 'locations';
+module.exports.MOVEMENTS = 'movements';
